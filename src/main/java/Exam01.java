7596efe411eaa1c4d36017133cb3601a5fb92260
kimyeon96 <flflfkfk@naver.com>
2022-03-16 11:05:11 +0900

    프로그래밍 실습 수업

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,8 @@
+public class Exam01 {
+    public static void main(String[] args) {
+        System.out.println("김연");
+        System.out.println("1996년 05월 11일");
+        System.out.println("인텔리제이");
+        System.out.println("안녕하세요");
+    }
+}
---
+++ src/main/java/Exam03_01.java
@@ -0,0 +1,18 @@
+public class Exam03_01 {
+    public static void main(String[] args) {
+        System.out.printf("%d / %d = %d", 100, 200, 1);
+        System.out.printf("\n");
+        System.out.printf("");
+
+        System.out.printf("%d\n", 123);
+        System.out.printf("%5d\n", 123);
+        System.out.printf("%05d\n", 123);
+
+        System.out.printf("%f\n", 123.45);
+        System.out.printf("%7.1f\n", 123.45);
+        System.out.printf("%7.3f\n", 123.45);
+
+        System.out.printf("%s\n", "Hi~Java");
+        System.out.printf("%10s\n","Hi~ Java");
+    }
+}
