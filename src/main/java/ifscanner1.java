9b0be4eee52a3ec389a790a60a68bc5f8944f712
kimyeon96 <flflfkfk@naver.com>
2022-04-04 19:41:38 +0900

    22.4.4

---
+++ src/main/java/ifscanner1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ifscanner1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a;
+
+        System.out.println("점수를 입력하세요");
+
+        if (a
+    }
+}
