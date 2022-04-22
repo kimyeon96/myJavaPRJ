011014208ba284ec31a8c3685f8081d0067cbc48
kimyeon96 <flflfkfk@naver.com>
2022-04-22 13:41:24 +0900

    22-04-22

---
+++ src/main/java/asfdadfa.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class asfdadfa {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a,b;
+        a = s.nextInt();
+        b = s.nextInt();
+
+        if (a<=b) {
+            System.out.printf("1");
+        } else {
+            System.out.printf("0");
+        }
+    }
+}
