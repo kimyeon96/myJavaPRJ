a3f859f079a54db73c0cd973e71694ae12da45e7
kimyeon96 <flflfkfk@naver.com>
2022-04-25 21:42:03 +0900

    22-04-25

---
+++ src/main/java/Pyramid2.java
@@ -0,0 +1,17 @@
+public class Pyramid2 {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        int k;
+        int num = 5;
+
+        for (i=0; i < num; i++) {
+            for (j=1; j<num-i; j++) {
+                System.out.print(" ");
+            } for (k=0; k<i*2+1; k++) {
+                System.out.print("*"); }
+             System.out.println();
+        }
+
+    }
+}
