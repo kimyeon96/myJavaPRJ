a67ce00e79873d83b5c24046882ddc59a2b3e938
kimyeon96 <flflfkfk@naver.com>
2022-05-16 21:38:34 +0900

    22-05-16

--- src/main/java/starpractice2.java
+++ src/main/java/starpractice2.java
@@ -1,20 +1,13 @@
-public class starpractice {
+public class starpractice2 {
     public static void main(String[] args) {
-
-        for (int i=1; i<=3; i++) {
-            for (int j=0; j<3-i; j++) {
-                System.out.printf(" ");
-            }
-            for ( int j = 1; j <= i*2-1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
         for (int i = 2; i >= 1; i--) {
             for (int j = 1; j <= 3 - i; j++) {
                 System.out.print(" ");
             }
+
+            // 하단 *
             for (int j = 1; j <= i * 2 - 1; j++) {
+
                 System.out.print("*");
             }
 
