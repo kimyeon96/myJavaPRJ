8f103b500e2b96dfdc8cf30b618f9bcb92328865
kimyeon96 <flflfkfk@naver.com>
2022-05-17 21:53:35 +0900

    22-05-17

---
+++ src/main/java/arraylist2.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class arraylist2 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(); // ArrayList로 메모리에 올림
+
+        // list 객체에 데이터 추가
+        list.add("이협건");
+        list.add("홍길동"); // 데이터 저장
+        list.add("이정훈");
+        list.add("김정엽");
+
+        int listSize = list.size(); //List의 데이터 수 구하기
+
+        for (int i=0; i<listSize; i++) { //일반 for문으로
+            System.out.println("이름 : " + list.get(i));
+        }
+    }
+}
