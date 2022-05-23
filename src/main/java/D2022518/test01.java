d03725cfa1355572a669c1103d0d9dab880075cd
kimyeon96 <flflfkfk@naver.com>
2022-05-23 20:24:36 +0900

    22-05-23

---
+++ src/main/java/D2022518/test01.java
@@ -0,0 +1,12 @@
+package D2022518;
+
+public class test01 {
+    public static void main(String[] args) {
+        String str = "IT CookBook. Java";
+        int len;
+
+        len = str.length();
+
+        System.out.printf("문자열 : %s");
+    }
+}
