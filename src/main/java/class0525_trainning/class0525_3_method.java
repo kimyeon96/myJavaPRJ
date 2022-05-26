72ed69386c31d3fee1d7d8cbd4e61ec8a1130d4e
kimyeon96 <flflfkfk@naver.com>
2022-05-26 20:05:32 +0900

    22-05-26

---
+++ src/main/java/class0525_trainning/class0525_3_method.java
@@ -0,0 +1,25 @@
+package class0525_trainning;
+
+public class class0525_3_method {
+    public static void main(String[] args) {
+        int i = 1, j = 10;
+
+        int k = sum(i, j);
+        System.out.println("k : " + k);
+
+        int m = sum(10, 11);
+        System.out.println("m : " + m);
+
+    }
+
+    public static int sum(int i1, int i2) {
+
+        int sum = 0;
+
+        for (int i = i1; i < i2; i++) {
+            sum += 1;
+
+        }
+        return sum;
+    }
+}
