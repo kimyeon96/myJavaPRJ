b9891af92f7e19cdaa4f84571adb47de096dfa8f
kimyeon96 <flflfkfk@naver.com>
2022-06-13 20:36:49 +0900

    22-06-13

---
+++ src/main/java/codeup/Ex_ch9_23.java
@@ -0,0 +1,22 @@
+package codeup;
+import java.util.Scanner;
+public class Ex_ch9_23 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String ss;
+        String tt = "";
+
+        int count, i;
+        System.out.printf("문자열을 입력하세요 : ");
+        ss = s.nextLine();
+
+        count = ss.length();
+
+        for (i = 0; i < count; i++) {
+            tt += ss.charAt(count - (i + 1));
+        }
+
+        System.out.printf("내용을 거꾸로 출력 ==> %s \n", tt);
+
+        }
+    }
