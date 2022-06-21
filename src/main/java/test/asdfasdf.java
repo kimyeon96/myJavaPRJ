1ee9e1b57ff31c504d5e425235ca1ff615084e32
kimyeon96 <flflfkfk@naver.com>
2022-06-21 21:47:52 +0900

    22.6.21

---
+++ src/main/java/test/asdfasdf.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class asdfasdf {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("이협건");
+        list.add("홍길동");
+
+        list.forEach(name -> System.out.println("name : " + name));
+        }
+    }
