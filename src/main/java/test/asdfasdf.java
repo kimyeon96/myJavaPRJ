b337ccc03aac41614267039a1fdeba4b1ae6be2d
kimyeon96 <flflfkfk@naver.com>
2022-06-21 21:48:31 +0900

    22.6.21

--- src/main/java/test/asdfasdf.java
+++ src/main/java/test/asdfasdf.java
@@ -6,8 +6,10 @@ import java.util.List;
 public class asdfasdf {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
-        list.add("이협건");
-        list.add("홍길동");
+        list.add("김연");
+        list.add("이정훈");
+        list.add("박우정");
+        list.add("김다운");
 
         list.forEach(name -> System.out.println("name : " + name));
         }
