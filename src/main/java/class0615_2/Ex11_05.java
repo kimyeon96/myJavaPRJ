dee5ab2aa8866416acff1c0db575978afb145ce2
kimyeon96 <flflfkfk@naver.com>
2022-06-27 23:14:05 +0900

    22.6.27

---
+++ src/main/java/class0615_2/Ex11_05.java
@@ -0,0 +1,13 @@
+package class0615_2;
+
+public class Ex11_05 {
+    public static void main(String[] args) {
+        Car myCar1 = new Car();
+
+        myCar1.upSpeed(100);
+        myCar1.upSpeed(150);
+
+        myCar1.downSpeed(50);
+        myCar1.downSpeed(160);
+    }
+}
