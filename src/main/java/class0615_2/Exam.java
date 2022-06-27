d8d31f760b948b7e4e2f1997ec573434f62b15e0
kimyeon96 <flflfkfk@naver.com>
2022-06-27 23:15:16 +0900

    22.6.27

---
+++ src/main/java/class0615_2/Exam.java
@@ -0,0 +1,15 @@
+package class0615_2;
+
+public class Exam {
+
+    public static void main(String[] args) {
+
+        ExamDTO eDTO = new ExamDTO();
+
+        eDTO.setTest("기말고사");
+        eDTO.setScore(100);
+
+        System.out.println("시험 : "+ eDTO.getTest());
+        System.out.println("점수 : "+ eDTO.getScore());
+    }
+}
